4908ef3161b29baef0d3ec19281808d1cb5998b6
Enoch Owoeye <enochowoeye001@gmail.com>
2022-04-04 16:17:21 +0100

    WellaLab1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Helloworldapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Enoch Owoeye, I am in Backend Development of Wella Schools!");
+            Console.WriteLine("I have joined the program to learn and improve on my skills, I have been a part of HNG durind the lockdown and since developed intrest, the ASUU strike afforded me the opportunity to be a part of this training and I do not take it for granted.");
+        }
+    }
+}
